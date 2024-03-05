117fbe84ac031998d6fe0c88776da2c9c18669e1
Cole Chapman <colechapman1993@gmail.com>
2024-03-05 09:12:18 -0800

    Added icons to the header toolbar

--- src/components/headerToolbar.tsx
+++ src/components/headerToolbar.tsx
@@ -1,5 +1,13 @@
-import { IonButtons, IonMenuButton, IonTitle, IonToolbar } from "@ionic/react";
+import {
+  IonButton,
+  IonButtons,
+  IonIcon,
+  IonMenuButton,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
 import { tenant } from "../tenant/tenantService";
+import { personCircle, locateOutline } from "ionicons/icons";
 
 const HeaderToolbar: React.FC = () => {
   return (
@@ -8,8 +16,25 @@ const HeaderToolbar: React.FC = () => {
       style={{ backgroundColor: tenant.theme.primaryColor }}
     >
       <IonButtons slot="start">
-        <IonMenuButton></IonMenuButton>
+        <IonMenuButton
+          style={{ color: tenant.theme.headerTextColor }}
+          autoHide={false}
+        ></IonMenuButton>
       </IonButtons>
+
+      <IonButtons slot="secondary">
+        {/* <IonButton>
+          <IonIcon slot="icon-only" icon={personCircle}></IonIcon>
+        </IonButton> */}
+        <IonButton>
+          <IonIcon
+            style={{ color: tenant.theme.headerTextColor }}
+            slot="icon-only"
+            icon={locateOutline}
+          ></IonIcon>
+        </IonButton>
+      </IonButtons>
+
       <IonTitle style={{ color: tenant.theme.headerTextColor }}>
         {tenant.appBarTitle}
       </IonTitle>
